448725a90af42292d77c80aa715b9378d83c55bf
Matz Momme Klint <matzmommeklint@gmail.com>
2021-06-05 01:35:55 +0200

    initial commit

---
+++ asmhack.py
@@ -0,0 +1,101 @@
+# dies ist ein Assembler für die Programmiersprache Hack
+# aus dem Buch "The Elements of Computing - building a modern computer from
+# first principles"
+import click
+from pathlib import Path
+
+SYMBOLS_BUILTIN = {
+    "SP": 0,
+    "LCL": 1,
+    "ARG": 2,
+    "THIS": 3,
+    "THAT": 4,
+    "R0": 0,
+    "R1": 1,
+    "R2": 2,
+    "R3": 3,
+    "R4": 4,
+    "R5": 5,
+    "R6": 6,
+    "R7": 7,
+    "R8": 8,
+    "R9": 9,
+    "R10": 10,
+    "R11": 11,
+    "R12": 12,
+    "R13": 13,
+    "R14": 14,
+    "R15": 15,
+    "SCREEN": 16384,
+    "KBD": 24576,
+}
+
+SYMBOLS_COMP = {
+    "0": "0101010",
+    "1": "0111111",
+    "-1": "0111010",
+    "D": "0001100",
+    "A": "0110000",
+    "!D": "0001101",
+    "!A": "0110001",
+    "-D": "0001111",
+    "-A": "0110011",
+    "D+1": "0011111",
+    "A+1": "0110111",
+    "D-1": "0001110",
+    "A-1": "0110010",
+    "D+A": "0000010",
+    "D-A": "0010011",
+    "A-D": "0000111",
+    "D&A": "0000000",
+    "D|A": "0010101",
+    "M": "1110000",
+    "!M": "1110001",
+    "-M": "1110011",
+    "M+1": "1110111",
+    "M-1": "1110010",
+    "D+M": "1000010",
+    "D-M": "1010011",
+    "M-D": "1000111",
+    "D&M": "1000000",
+    "D|M": "1010101",
+}
+
+SYMBOLS_DEST = {
+    "null": "000",
+    "M": "001",
+    "D": "010",
+    "MD": "011",
+    "A": "100",
+    "AM": "101",
+    "AD": "110",
+    "AMD": "111",
+}
+
+SYMBOLS_JUMP = {
+        "null" : "000",
+        "JGT" : "001",
+        "JEQ" : "010",
+        "JGE" : "011",
+        "JLT" : "100",
+        "JNE" : "101",
+        "JLE" : "110",
+        "JMP" : "111",
+
+
+@click.command()
+@click.argument(
+    "asm-file", type=click.Path(exists=True, file_okay=True, dir_okay=False)
+)
+def main(asm_file):
+    asm_file = Path(asm_file)
+    if not asm_file.suffix == "asm":
+        print("given file is not an asm-file")
+        return 1
+    asm: str = ""
+    with open(asm_file, "r") as f:
+        asm = f.read()
+
+
+if __name__ == "__main__":
+    main()
